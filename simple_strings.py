f59f02d252c390a5063609e7c52f4c341405c450
manuelesteve <mesteve.ieu2023@student.ie.edu>
2025-01-28 16:46:37 +0100

    added simple string examples

---
+++ simple_strings.py
@@ -0,0 +1,12 @@
+s1 = "john" # double quote
+s2 = 'mary' # single quote
+
+print(s1)
+print(s2)
+
+s1 = ("And then he asked: 'how are you?'")
+print(s1)
+s1 = ('And then he asked: "how are you?"')
+print(s1)
+s1 = "This is how to have \" and \'inside a string" # you can also escape them
+print(s1)
